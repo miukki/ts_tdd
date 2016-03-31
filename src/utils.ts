1955b794834f83e0f4c99219b52646f09e42125d
Anja Ishmukhametova <miukki@gmail.com>
2016-03-31 22:32:27 +0800

    issue#1

--- src/utils.ts
+++ src/utils.ts
@@ -14,9 +14,27 @@ module utils {
     export function isObject(some: any): boolean {
         return toString.call(some) === types.object;
     }
-    
     export function isEmpty(some: any): boolean {
         return some == null;
     }
-    
+    export function isString(some: any): boolean {
+        return typeof some === 'string' || some instanceof String;
+    }
+    export function isNumber(some: any): boolean {
+        return typeof some === 'number' && some === some;
+    }
+    export function isNaN(some: any): boolean {
+        return typeof some === 'number' && some !== some;
+    }
+    export function isArray(some: any): boolean {
+        return typeof some === 'object' && Array.isArray(some);
+    }
+    export function isNull(some: any): boolean {
+        return some === null;
+    }
+    export function isUndefined(some: any): boolean {
+        return some === undefined;
+    }
+
+
 }
--- test/utils.ts
+++ test/utils.ts
@@ -18,7 +18,45 @@ new TestManager([
                 trueValues: [null, undefined],
                 falseValues: [0, '', []],
                 testCallback: utils.isEmpty
+            },
+            {
+                testName: 'isString',
+                trueValues: ['', 'some'],
+                falseValues: [null, undefined, []],
+                testCallback: utils.isString
+            },
+            {
+                testName: 'isNumber',
+                trueValues: [0, 5],
+                falseValues: ['', undefined, []],
+                testCallback: utils.isNumber
+            },
+            {
+                testName: 'isNaN',
+                trueValues: [NaN],
+                falseValues: ['', undefined, [], null],
+                testCallback: utils.isNaN
+            },
+            {
+                testName: 'isArray',
+                trueValues: [[],new Array(), ['',]],
+                falseValues: ['', undefined, null],
+                testCallback: utils.isArray
+            },
+            {
+                testName: 'isNull',
+                trueValues: [null],
+                falseValues: ['', undefined],
+                testCallback: utils.isNull
+            },
+            {
+                testName: 'isUndefined',
+                trueValues: [undefined],
+                falseValues: [null, ''],
+                testCallback: utils.isUndefined
             }
+
+
         ]
     }
 ]);
