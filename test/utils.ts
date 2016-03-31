8c23ef64567ad218f664981d8a966adbdfa6af0e
daniil <tsddaniil@gmail.com>
2016-03-31 22:19:40 +0300

    add test manager

--- test/utils.ts
+++ test/utils.ts
@@ -1,28 +1,24 @@
 ///<reference path="../typings/tsd.d.ts"/>
 ///<reference path="../dist/ts_project.d.ts"/>
 
-
-describe('utils', () => {
-    
-    it('isObject', () => {
-        
-        expect(utils.isObject({})).to.be(true);
-        /* tslint:disable */
-        expect(utils.isObject(new Number(1))).to.be(false);
-        /* tslint:enable */
-        expect(utils.isObject([])).to.be(false);
-        expect(utils.isObject(1)).to.be(false);
-        
-    });
-    
-    it('isEmpty', () => {
-        
-        expect(utils.isEmpty(null)).to.be(true);
-        expect(utils.isEmpty(null)).to.be(true);
-        expect(utils.isEmpty(undefined)).to.be(true);
-        expect(utils.isEmpty(0)).to.be(false);
-        expect(utils.isEmpty('')).to.be(false);
-        
-    });
-    
-});
+new TestManager([
+    {
+        testName: 'utils',
+        children: [
+            {
+                testName: 'isObject',
+                /* tslint:disable */
+                trueValues: [{}, new Object()],
+                falseValues: [new Number(0), new String(''), [], 1, ''],
+                testCallback: utils.isObject
+                /* tslint:enable */
+            },
+            {
+                testName: 'isEmpty',
+                trueValues: [null, undefined],
+                falseValues: [0, '', []],
+                testCallback: utils.isEmpty
+            }
+        ]
+    }
+]);
